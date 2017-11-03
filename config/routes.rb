b715397164445979beb3643af30130f038f60e0c
Aleksey Zolotov <myxrome@gmail.com>
2017-11-03 12:17:09 +0400

    MOON-7 #done - список сценариев на главной странице.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :scenes
-  resources :stages
-  resources :scenarios
+  resources :stages do
+    resources :scenes, only: [:index]
+  end
+  resources :scenarios do
+    resources :stages, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
